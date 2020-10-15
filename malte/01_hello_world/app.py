ae2f1de1bb6421af7f3cecb43df20ba0defe8aaa
Malte Øhlers <malte@greenbyte.com>
2020-10-15 22:01:07 +0200

    Malte: Hello world

---
+++ malte/01_hello_world/app.py
@@ -0,0 +1,14 @@
+print(
+"""
+-----------------------------------
+            HELLO APP
+-----------------------------------
+
+"""
+)
+
+name = input("What's your name, dude? ")
+print(f"Hello {name}-dude, awesome to meet you.")
+
+
+
